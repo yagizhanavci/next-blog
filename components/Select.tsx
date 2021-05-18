c3ec4d5ca53ea160a76616d6a5f84a6ada2a52cf
Yağızhan Avcı <yagizhan.avci@crosstech.com.tr>
2021-05-18 12:43:54 +0300

    Expanded width of select component

--- components/Select.tsx
+++ components/Select.tsx
@@ -28,11 +28,11 @@ export const Select: React.FC<ISelectProps> = ({
       <Listbox value={value} onChange={onChange}>
         {({ open }) => (
           <React.Fragment>
-            <Listbox.Button className="flex items-center pl-4 bg-white border border-gray-300 rounded-md appearance-none dark:border-gray-900 focus:ring-2 focus:ring-indigo-500 focus:outline-none dark:bg-gray-800 dark:text-gray-100">
+            <Listbox.Button className="dropdown__button">
               {value
                 ? options.find((option) => option.value === value).label
                 : placeholder}
-              <CaretIcon className="w-2 h-2 m-4 pointer-events-none" />
+              <CaretIcon className="dropdown__button-caret" />
             </Listbox.Button>
             {open && (
               <Listbox.Options
@@ -41,7 +41,7 @@ export const Select: React.FC<ISelectProps> = ({
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ type: "spring", duration: 0.8 }}
                 static
-                className="absolute z-10 w-full py-1 mt-1 bg-white border-2 border-t-0 border-gray-300 shadow-lg max-h-60overflow-auto dark:bg-black dark:border-gray-900 focus:outline-none focus:ring-offset-white dark:focus:ring-offset-black"
+                className="dropdown__options"
               >
                 {options.map((option) => (
                   <Listbox.Option
@@ -58,26 +58,14 @@ export const Select: React.FC<ISelectProps> = ({
                           active
                             ? "bg-indigo-500 dark:bg-indigo-500"
                             : "bg-white dark:bg-black",
-                          "min-w-full",
-                          "hover:text-white text-black dark:text-white",
-                          "block",
-                          "cursor-pointer",
-                          "border-2",
-                          "border-transparent",
-                          "flex",
-                          "items-center",
-                          "px-2",
-                          "py-2",
-                          "text-sm",
-                          "text-gray-700",
-                          "whitespace-nowrap",
+                          "dropdown__option",
                         )}
                       >
                         {option.label}
                         {selected ? (
-                          <CheckIcon className="w-4 h-4 ml-4 text-black dark:text-white" />
+                          <CheckIcon className="dropdown__option-check" />
                         ) : (
-                          <div className="w-4 h-4 ml-4" />
+                          <div className="dropdown__option-check-filler" />
                         )}
                       </a>
                     )}
